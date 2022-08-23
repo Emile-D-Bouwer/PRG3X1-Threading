7a4c5eb26dc647bb98ee8a1134a514148d02de8f
amobrian11 <67075974+amobrian11@users.noreply.github.com>
2022-08-23 09:46:50 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,27 @@
 package com.basepackage;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	System.out.println("What course you want to register? (Diploma/Degree)");
+        Scanner input = new Scanner(System.in);
+        String type = input.nextLine();
+	    switch (type) {
+            case "diploma":
+          
+                break;
+
+            case "degree":
+            
+                break;
+
+            default:
+                System.out.println("==== Input is invalid ====");
+                type = input.nextLine();
+                break;
+        }
+        input.close();
     }
 }
