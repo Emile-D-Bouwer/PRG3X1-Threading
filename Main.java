654b8108f5af9322546e5966f4ac4ae0b19a7467
amobrian11 <67075974+amobrian11@users.noreply.github.com>
2022-08-23 10:01:41 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,16 +5,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	Student st = new student(); 
 	System.out.println("What course you want to register? (Diploma/Degree)");
         Scanner input = new Scanner(System.in);
         String type = input.nextLine();
 	    switch (type) {
             case "diploma":
-          
+                    //st.Diploma();
                 break;
 
             case "degree":
-            
+                  //st.Degree();
                 break;
 
             default:
@@ -23,5 +24,6 @@ public class Main {
                 break;
         }
         input.close();
+	//st.Print();
     }
 }
